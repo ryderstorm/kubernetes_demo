735cf289dcec6bde928375118154194d5ade7362
Damien Storm <damien.r.storm@gmail.com>
2023-07-05 02:18:48 -0400

    Refactor responsiveness tests
    - Refactored the responsiveness tests to use a loop
    - Updated the test descriptions to be more descriptive
    - Added timeout to HTTP requests
    - Added additional tests for the timestamp server health and version endpoints

--- spec/responsiveness_spec.rb
+++ spec/responsiveness_spec.rb
@@ -11,44 +11,45 @@ raise 'CLUSTER_ENDPOINT environment variable must be set to run integration test
 puts "Running integration tests for demo apps running in the Kubernetes cluster at:\n#{cluster_endpoint.blue}"
 
 describe 'Demo App Integration Tests' do
-  it 'tests responsiveness of [traefik] demo appliation' do
-    url = "http://traefik.#{cluster_endpoint}"
-    response = HTTParty.get(url)
-    expect(response.code).to eq(200)
-  end
-
-  it 'tests responsiveness of [whoami] demo appliation' do
-    url = "http://whoami.#{cluster_endpoint}"
-    response = HTTParty.get(url)
-    expect(response.code).to eq(200)
-  end
-
-  it 'tests responsiveness of [nginx-hello] demo appliation' do
-    url = "http://nginx-hello.#{cluster_endpoint}"
-    response = HTTParty.get(url)
-    expect(response.code).to eq(200)
-  end
-
-  it 'tests responsiveness of [ubuntu-testbed] demo appliation' do
-    url = "http://ubuntu-testbed.#{cluster_endpoint}"
-    response = HTTParty.get(url)
-    expect(response.code).to eq(200)
-  end
-
-  it 'tests responsiveness of [timestamp] demo appliation' do
-    url = "http://timestamp.#{cluster_endpoint}"
-    response = HTTParty.get(url)
-    expect(response.code).to eq(200)
+  app_domains = %w[traefik whoami game nginx-hello ubuntu-testbed timestamp]
+  app_domains.each do |app_domain|
+    it "tests that the #{app_domain} app is running" do
+      url = "http://#{app_domain}.#{cluster_endpoint}"
+      puts "\n  Testing url: #{url.blue}"
+      response = HTTParty.get(url, timeout: 2)
+      expect(response.code).to eq(200), "Expected #{url.blue} to return 200, got #{response.code} | #{response.body}"
+    end
   end
 
   it 'tests that the timestamp server returns a valid JSON response' do
     url = "http://timestamp.#{cluster_endpoint}"
-    response = HTTParty.get(url)
+    response = HTTParty.get(url, timeout: 2)
+    expect(response.code).to eq(200)
+    puts "\n  Testing url: #{url.blue}"
     body = JSON.parse(response.body)
     expect(body['message']).to eq('Automate all the things!')
     server_time = Time.at(body['timestamp'])
     expect(server_time).to be_within(30).of(Time.now)
     expect(body['timestamp'].to_s).to match(/\d{10}/)
+  end
+
+  it 'tests that the timestamp server health endpoint returns a valid JSON response' do
+    url = "http://timestamp.#{cluster_endpoint}/health"
+    response = HTTParty.get(url, timeout: 2)
+    puts "\n  Testing url: #{url.blue}"
+    expect(response.code).to eq(200)
+    expect(response.body).to eq('OK')
+  end
+
+  it 'tests that the timestamp server version endpoint returns a valid JSON response' do
+    url = "http://timestamp.#{cluster_endpoint}/version"
+    response = HTTParty.get(url, timeout: 2)
+    puts "\n  Testing url: #{url.blue}"
+    body = JSON.parse(response.body)
+    expect(response.code).to eq(200), "Expected #{url.blue} to return 200, got #{response.code} | #{response.body}"
+    expect(body['version']).to match(/^[a-f0-9]{7}$/)
+    expect(body['build_time']).to match(/\d{10}/)
+    expect(body['pretty_build_time']).to match(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} UTC/)
   end
 end
 
